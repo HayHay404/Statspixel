d5865307eaa68668768975d390637ec042a1adfc
Pixwl <pixwl@pixwl.dev>
2022-08-06 14:45:29 +0930

    Add comments and other shit

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,67 @@
-import axios from "axios";
 import dotenv from "dotenv"
-import tmi from "tmi.js"
 
-dotenv.config({path: "../.env"})
+// Best practice (imo) to put this at the top of the file. Incase any imports needs to access .env
+dotenv.config({path: path.resolve(__dirname, '..', '.env')})
+
+import axios from "axios"
+import tmi from "tmi.js"
+import path from 'path'
 
 const client = tmi.Client({
     options: {
-        clientId: process.env["CLIENT_ID"]
+        clientId: process.env["CLIENT_ID"],
+        debug: process.env.NODE_ENV === 'development' // Sets to true if the NODE_ENV environment variable is set to 'development'
     },
     identity: {
-        username: "HypixelStats",
+        username: "Statspixel", // hehe should be Statspixwl
         password: process.env["CLIENT_OAUTH"] as string,
     },
-    channels: ["HayHayIsLive", "Cakadyboi"]
+    channels: ["HayHayIsLive", "Cakadyboi"],
 })
 
-async function connect() {
-    await client.connect()
-}
+/**
+ * Called when the bot connects to a Twitch server
+ */
+client.on('connected', (address, port) => {
+    console.log(`Connected to ${address}:${port}`)
+})
 
-
+/**
+ * Called when the bot recieves a message from a channel
+ */
 client.on("chat", async (channel, userState, message, self) => {
-    
+    // Prevent messages from self from being parsed
+    if (self) return;
+
     if (!message.startsWith("h!")) return;
-    const arr = message.split(" ")
-    const data = await getHypixelInfo(arr[2]);
+    // Remove the "h!" and split the message into an array of words
+    // Also makes it so that you don't need to put a space after the prefix
+    const args = message.slice("h!".length).split(" ")
+    if (args.length < 2) return; // Invalid command
+
+    const data = await getHypixelInfo(args[1]);
 
     if (data == null) return;
 
-    const response = parseData(data, arr[1])
+    const response = parseData(data, args[0])
 
     if (response == null) {
         return client.say(channel, "User not found. Please try again later.")
+    } else if (response == undefined) {
+        return client.say(channel, "That player does not exist.")
     }
 
-    return client.say(channel, `Stats for ${arr[2]}: Wins: ${response["wins"]}, Losses: ${response["losses"]}, Winstreak: ${response["winstreak"]}, FKDR: ${response["fkdr"]}`)
-
+    // You might want to make the message just what the function returns
+    // For some gamemodes, it probably won't be the same types of data (e.g. survival)
+    return client.say(channel, `Stats for ${args[1]}: Wins: ${response["wins"]}, Losses: ${response["losses"]}, Winstreak: ${response["winstreak"]}, FKDR: ${response["fkdr"]}`)
 })
 
+/**
+ * Parses data from the Hypixel API
+ * @param data The data from the Hypixel API
+ * @param game The game that the user is requesting
+ * @returns {Object} The data that will be sent to the channel
+ */
 function parseData(data : any, game : string) {
     let response = null;
 
@@ -45,7 +69,7 @@ function parseData(data : any, game : string) {
         case "bw" || "bedwars":
             data = data["stats"]["Bedwars"]
             const fkdr = (data["final_kills_bedwars"] / data["final_deaths_bedwars"]).toPrecision(4);
-            
+
             response = {
                 "fkdr": fkdr,
                 "losses": data["losses_bedwars"],
@@ -54,7 +78,7 @@ function parseData(data : any, game : string) {
             }
             // console.log(response)
             break;
-    
+
         default:
             response = null;
             break;
@@ -63,6 +87,11 @@ function parseData(data : any, game : string) {
     return response;
 }
 
+/**
+ * Retrieves the UUID for a player by their in-game name
+ * @param name The username of the player
+ * @returns The UUID for the given player
+ */
 async function getUUID(name : string) {
     let uuid;
 
@@ -77,11 +106,16 @@ async function getUUID(name : string) {
     return uuid;
 }
 
+/**
+ * Retrieves player data from the Hypixel API
+ * @param name The username of the player
+ * @returns {Object} The data from the Hypixel API
+ */
 async function getHypixelInfo(name : string) {
 
     const uuid = await getUUID(name)
     if (uuid == null) {
-        return null;   
+        return undefined;
     }
 
     try {
@@ -100,5 +134,11 @@ async function getHypixelInfo(name : string) {
     }
 }
 
-// getUUID("HayHayIsALoser")
-connect()
+/**
+ * Main function used for connecting to Twitch
+ */
+async function main() {
+    await client.connect()
+}
+
+main()
